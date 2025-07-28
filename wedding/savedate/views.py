7a1a1016d894631d6f2fda9d0ca80e8ee491f5ba
tim c <me@timc.space>
2025-07-28 17:26:35 -0400

    replace rsvp embed

--- wedding/savedate/views.py
+++ wedding/savedate/views.py
@@ -13,7 +13,7 @@ def notion_page(request, page):
              'gallery': """<iframe src="https://notjessytang.notion.site/ebd/219c2a181f02809bb911d9d22f373e3a" width="100%" height="900" frameborder="0" allowfullscreen />""",
              'travel': """<iframe src="https://notjessytang.notion.site/ebd/219c2a181f0280ff97d2c8430660ce2b" width="100%" height="900" frameborder="0" allowfullscreen />""",
              'details': """<iframe src="https://notjessytang.notion.site/ebd/219c2a181f028057a6cad4c09dde51c0" width="100%" height="900" frameborder="0" allowfullscreen />""",
-             'rsvp': """<div data-tf-live="01K0R0P9DWNDWCF42TNDTRD6EX"></div><script src="//embed.typeform.com/next/embed.js"></script>""",}
+             'rsvp': """<iframe src="https://iu.co1.qualtrics.com/jfe/form/SV_3BE0DYMaghtLUmq" width="100%" height=900" frameborder="0" allowfullscreen />""",}
     try:
         html = pages[page]
     except KeyError:
